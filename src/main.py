ccbd2181e8ec7ac58c65f7d49e06ce15fc1ce029
xuanluo2 <xuanluo2@illinois.edu>
2023-11-24 21:52:50 -0800

    plot results

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 import pytoml
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import uniform
 from meta.parameter_tuning.okapi_ndcg_estimator import OkapiBM25NdcgEstimator
 from meta.parameter_tuning.pivoted_length_ndcg_estimator import PivotedLengthNdcgEstimator
@@ -13,6 +14,16 @@ from meta.estimator_search.estimator_search_wrapper import EstimatorSearchWrappe
 from meta.utils.process_configs import process_query_cfg, process_param_tuner_cfg
 from meta.utils.eval_utils import train_test_split
 
+
+ESTIMATOR_TO_PARAM_GRID = {
+    OkapiBM25NdcgEstimator:  {'k1': np.linspace(1.6, 1.8, num=20), 'b': np.linspace(0.7, 0.8, num=20)},
+    PivotedLengthNdcgEstimator: {'s': np.linspace(0, 8, num=200)},
+    AbsoluteDiscountNdcgEstimator: {'delta': np.linspace(0, 8, num=200)},
+    JelinekMercerNdcgEstimator: { 'l': np.linspace(0, 8, num=200)},
+    DirichletPriorNdcgEstimator: {'mu': np.linspace(0, 5000, num=200)}
+}
+
+SEARCH_TYPES = ['grid', 'random']
 
 
 if __name__ == '__main__':
@@ -32,26 +43,34 @@ if __name__ == '__main__':
 
     wrapper = EstimatorSearchWrapper(cfg, query_start_train, query_start_test, queries_train, queries_test, cv, logging_level)
 
-    # OkapiBM25NdcgEstimator
-    okapi_param_grid = {'k1': np.linspace(1.6, 1.8, num=20), 'b': np.linspace(0.7, 0.8, num=20)}
-    wrapper.perform_search(OkapiBM25NdcgEstimator, okapi_param_grid, search_type='grid')
-    wrapper.perform_search(OkapiBM25NdcgEstimator, okapi_param_grid, search_type='random')
 
-    # PivotedLengthNdcgEstimator
-    pl_param_grid = {'s': np.linspace(0, 8, num=200)}
-    wrapper.perform_search(PivotedLengthNdcgEstimator, pl_param_grid, search_type='grid')
-    wrapper.perform_search(PivotedLengthNdcgEstimator, pl_param_grid, search_type='random')
+    test_scores = []
+    model_identifiers = []
 
-    # AbsoluteDiscountNdcgEstimator
-    ad_param_grid = {'delta': np.linspace(0, 8, num=200)}
-    wrapper.perform_search(AbsoluteDiscountNdcgEstimator, ad_param_grid, search_type='grid')
-    wrapper.perform_search(AbsoluteDiscountNdcgEstimator, ad_param_grid, search_type='random')
+    overall_best_estimator = None
+    overall_best_model = None
+    overall_best_test_score = 0
+    for estimator in ESTIMATOR_TO_PARAM_GRID:
+        param_grid = ESTIMATOR_TO_PARAM_GRID[estimator]
+        for search_type in SEARCH_TYPES:
+            best_model, test_score = wrapper.perform_search(estimator, param_grid, search_type=search_type)
+            test_scores.append(test_score)
+            model_identifiers.append(estimator.__name__ + "-" + search_type)
+            if test_score > overall_best_test_score:
+                overall_best_estimator = estimator
+                overall_best_model = best_model
+                overall_best_test_score = test_score
 
-    # # Define the parameter grid
-    jm_param_grid = { 'l': np.linspace(0, 8, num=200)}
-    wrapper.perform_search(JelinekMercerNdcgEstimator, jm_param_grid, search_type='grid')
-    wrapper.perform_search(JelinekMercerNdcgEstimator, jm_param_grid, search_type='random')
+    print("Overall best model: " + str(overall_best_model))
+    print("Best Test score: "  + str(overall_best_test_score))
 
-    dp_param_grid = {'mu': np.linspace(0, 5000, num=200)}
-    wrapper.perform_search(DirichletPriorNdcgEstimator, dp_param_grid, search_type='grid')
-    wrapper.perform_search(DirichletPriorNdcgEstimator, dp_param_grid, search_type='random')
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.bar(model_identifiers, test_scores, color='skyblue')
+    plt.xlabel('Model Identifier')
+    plt.ylabel('Test Score')
+    plt.title('Test Scores for Each Iteration')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('results.png')
+
